d7c906f292444d4f0e6b2f82044e99ca42c56703
Mohanapriya20 <mohanagesan97@gmail.com>
2017-06-08 18:07:12 +0530

    Create Multi.java

---
+++ Multi.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+class Multi
+{
+ public static void main(String args[])
+{
+ String c="HELLO";
+  for(int i=1;i<=5;i++)
+{
+    
+System.out.println(c);
+}
+}
+} 
